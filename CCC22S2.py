d6f0dd46461f50ed10e37a2f64c451b9602bccab
Ian Shi <72175372+lifeablestuff@users.noreply.github.com>
2024-02-19 19:52:38 -0800

    Update CCC22S2.py
    should be functioning as intended

--- CCC22S2.py
+++ CCC22S2.py
@@ -15,7 +15,7 @@ for x in range(len(info)):
         for y in info[x+1:int(info[x])+x+1]:
             temp.append(y)
         groups.append(temp)
-        
+
 
 samegroup.append(groups[0])
 seperated.append(groups[1])
@@ -35,30 +35,36 @@ same = [['A','B'],['G','L'],['J','K']]
 seper = [['D','F'],['D','G']]
 finalized = [['A C G'],['B D F'],['E H I'],['J K L']]
 '''
+temp = []
 violations = 0
 same_violated = False
 seper_violated = False
-
+confirm = False
 for x in finalized:
-    temp = x[0].split(' ')
-    
+    temp.append(x[0].split(' '))
+
 for y in same:
-    for s in finalized:
-        if y[0] not in temp:
-            if y[1] not in temp:
-            
-                same_violated = True
+    for s in temp:
+        if y[0] in s and y[1] in s:
+            same_violated = False
+            break
+        else:
+            same_violated = True
+     
     if same_violated == True:
         violations += 1
+confirm = False
 
 for z in seper:
-    for g in finalized:
-        if z[0] in temp:
-            if z[1] in temp:
+    for g in temp:
+        if z[0] in g and z[1] in g:
                 seper_violated = True
-    
+                break
+        else:
+            seper_violated = False
     if seper_violated == True:
-        violations +=1
+        violations += 1
+        
 
 
 print(violations)
